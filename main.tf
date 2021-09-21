52f103a09e5704361c728048403e8f1152a0ce2c
Yogesh Chhetri <36565350+yogeshCt3@users.noreply.github.com>
2021-09-21 20:09:14 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "lamp-stack" {
+  name         = "yogesh"
+  machine_type = "e2-standard-2"
+  zone         = "us-central1-c"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20210825"   
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+  
+}
+
