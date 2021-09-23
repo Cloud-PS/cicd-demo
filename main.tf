43d129b03ccd6f02283943c5c47154df6ff9e2e8
Yogesh Chhetri <36565350+yogeshCt3@users.noreply.github.com>
2021-09-23 14:10:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,18 +6,35 @@ provider "google" {
 
 
 resource "google_compute_instance" "lamp-stack" {
-  name         = "yogesh-2"
+  name         = "yogesh"
   machine_type = "e2-standard-2"
   zone         = "us-central1-c"
-
+  tags = ["firewall-http"]
   boot_disk {
     initialize_params {
       image = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20210825"   
+      size = 20
+      type = "pd-standard"
     }
   }
   network_interface {
     network = "default"
+    access_config {
+      // empty access_config assigns a public ip to the instance
+    }
   }
-  
+  metadata_startup_script = "sudo apt update -y && sudo apt install apache2 -y && cd /var/www/html && sudo su && rm index.html && echo \"<title> Hello </title>  <body> hello </body>\"  > index.html && echo done"
 }
 
+
+resource "google_compute_firewall" "http" {
+  name    = "firewall-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  target_tags   = ["firewall-http"]
+}
